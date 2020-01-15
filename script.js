3289993af2748fcff00bc0e2bf3fccb420b17f8b
Code-Plus-Coffee <m416a@protonmail.com>
2020-01-15 09:41:16 +0530

    Optimized assets.

--- script.js
+++ script.js
@@ -2,21 +2,24 @@ const scrollButton = document.getElementById("scroll-top-btn");
 
 // On scrolling, the button will be displayed
 window.onscroll = () => {
-    scrollToTop()
+    scrollToTop();
 };
 
 // Show when scroll state is greater than 20
 const scrollToTop = () => {
-    if(document.body.scrollTop > 20 
-        || document.documentElement.scrollTop > 20) {
-            scrollButton.style.display = "block";
-        } else {
-            scrollButton.style.display = "none";
-        }
-}
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        scrollButton.style.display = "block";
+    } else {
+        scrollButton.style.display = "none";
+    }
+};
 
 // Set the scroll state 0
 const setScrollState = () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-}
+    window.location.hash = "";
+};
